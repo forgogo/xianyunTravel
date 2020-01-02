22d93d592b719a33dda5d680cfb115f8b14d7922
Wuchuning <forgogo3933@gmail.com>
2020-01-02 16:08:48 +0800

    机票页组件创建

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -1,58 +1,52 @@
 <template>
-    <section class="contianer">
-        <el-row  type="flex" justify="space-between">
+  <section class="contianer">
+    <el-row type="flex" justify="space-between">
+      <!-- 顶部过滤列表 -->
+      <div class="flights-content">
+        <!-- 过滤条件 -->
+        <div></div>
 
-            <!-- 顶部过滤列表 -->
-            <div class="flights-content">
-                <!-- 过滤条件 -->
-                <div>
-                    
-                </div>
-                
-                <!-- 航班头部布局 -->
-                <div>
-                    
-                </div>
-                
-                
-                <!-- 航班信息 -->
-                <div>
-                    
-                </div>
-            </div>
+        <!-- 航班头部布局 -->
+        <FlightsListHead />
 
-            <!-- 侧边栏 -->
-            <div class="aside">
-                <!-- 侧边栏组件 -->
-            </div>
-        </el-row>
-    </section>
+        <!-- 航班信息 -->
+        <div></div>
+      </div>
+
+      <!-- 侧边栏 -->
+      <div class="aside">
+        <!-- 侧边栏组件 -->
+      </div>
+    </el-row>
+  </section>
 </template>
 
 <script>
-
+import FlightsListHead from "@/components/air/flightsListHead.vue";
 import moment from "moment";
 
 export default {
-    data(){
-        return {
-        }
-    }
-}
+  data() {
+    return {};
+  },
+  components: {
+    FlightsListHead
+  }
+};
 </script>
 
 <style scoped lang="less">
-    .contianer{
-        width:1000px;
-        margin:20px auto;
-    }
+.contianer {
+  width: 1000px;
+  margin: 20px auto;
+}
 
-    .flights-content{
-        width:745px;
-        font-size:14px;
-    }
+.flights-content {
+  width: 745px;
+  font-size: 14px;
+}
 
-    .aside{
-        width:240px;
-    } 
+.aside {
+  width: 240px;
+}
 </style>
